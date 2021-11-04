a1a2d804967873234fd4ee5e74c95bf9aa825c6f
katiegolder <kxgolder@bu.edu>
2021-11-04 17:09:40 -0400

    simple button example

---
+++ ShazApp/lib/sample_second_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: new WaterButton()
+    )
+  );
+}
+
+class WaterButton extends StatefulWidget {
+  @override
+  WaterButtonState createState() => new WaterButtonState();
+}
+
+class WaterButtonState extends State<WaterButton> {
+
+  String message = "";
+  void onPressed() {
+    setState(() {
+      message = "Water Button Pressed!";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Shazamboni"), backgroundColor: Colors.lightBlue,),
+        body: new Container(
+          child: new Center(
+            child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(message),
+                new RaisedButton(
+                child: new Text("Water"),
+                color: Colors.blue,
+                onPressed: onPressed
+                )
+           ]
+         )
+        )
+      )
+    );
+  }
+}
