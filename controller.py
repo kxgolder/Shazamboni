d2767b49dc943e75636c5014bea3192c42c81c56
Yanni Pang <yanni@bu.edu>
2022-02-24 17:38:42 -0500

    modify angles for forward and back

--- controller.py
+++ controller.py
@@ -12,31 +12,24 @@ s.listen(5)
 print("socket is listening")
 
 
-def servo(degrees):
-    print(f"set wheels to {degrees}")
-
-
-def set_wheels(degrees):
-    servo(degrees)
-
-
 def set_speed(degrees, distance):
-    if degrees == 0:
+    if distance == 0:
         motor.stop()
         print(f"stop")
-    elif degrees > 180:
+    elif 90 <= degrees <= 270:
         motor.backward()
         print(f"go backward")
 
-    elif degrees < 180:
+    elif 90 > degrees > 270:
         motor.forward()
         print(f"go forward")
+
 
 while True:
     c, addr = s.accept()
     print(f"Connected to {addr}")
     c.send("OK".encode())
-    while(1):
+    while (1):
         try:
             result = json.loads(c.recv(1024).decode())
             print(result)
@@ -45,8 +38,7 @@ while True:
         # x = result["distance"]*math.cos(result["degrees"])
         # y = result["distance"] * math.sin(result["degrees"])
         # print(f"x: {x}, y:{y}")
-        set_wheels(result["degrees"])
         set_speed(result["degrees"], result["distance"])
 
-    #c.close()
+    # c.close()
     # break
