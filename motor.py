afe571a5f465dc00ef2bb23a625c28f8e212107d
Yanni Pang <yanni@bu.edu>
2022-02-24 23:57:40 -0500

    modify to use differential driving

--- motor.py
+++ motor.py
@@ -1,7 +1,10 @@
 # Python Script
 # https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
 
+import math
 import RPi.GPIO as GPIO
+import differential_drive as d
+
 from time import sleep
 
 # in1 = 24  # pin 24
@@ -18,7 +21,6 @@ right_motor_parameters = {
     "in": [26, 27],
     "en": 28
 }
-SPEED = 75
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(left_motor_parameters["in"] + right_motor_parameters["in"], GPIO.OUT)
@@ -30,18 +32,31 @@ right_motor = GPIO.PWM(right_motor_parameters["en"], 1000)
 
 left_motor.start(25)
 right_motor.start(25)
+MAX_JOYSTICK = 1
+MIN_JOYSTICK = -1
+MAX_SPEED = 100
+MIN_SPEED = -100
 
 
-def forward():
-    GPIO.output(left_motor_parameters["in"][0], GPIO.HIGH)
-    GPIO.output(left_motor_parameters["in"][1], GPIO.LOW)
-    left_motor.ChangeDutyCycle(SPEED)
+def drive(degrees, distance):
+    right, left = d.joystickToDiff(degrees, distance, MIN_JOYSTICK, MAX_JOYSTICK, MIN_SPEED, MAX_SPEED)
+    if right > 0:
+        GPIO.output(right_motor_parameters["in"][0], GPIO.HIGH)
+        GPIO.output(right_motor_parameters["in"][1], GPIO.LOW)
+    elif right < 0:
+        GPIO.output(right_motor_parameters["in"][0], GPIO.LOW)
+        GPIO.output(right_motor_parameters["in"][1], GPIO.HIGH)
+    else:
+        GPIO.output(right_motor_parameters["in"], GPIO.LOW)
 
+    if left > 0:
+        GPIO.output(left_motor_parameters["in"][0], GPIO.HIGH)
+        GPIO.output(left_motor_parameters["in"][1], GPIO.LOW)
+    elif left < 0:
+        GPIO.output(left_motor_parameters["in"][0], GPIO.LOW)
+        GPIO.output(left_motor_parameters["in"][1], GPIO.HIGH)
+    else:
+        GPIO.output(left_motor_parameters["in"], GPIO.LOW)
 
-def stop():
-    GPIO.output(left_motor_parameters["in"], GPIO.LOW)
-
-
-def backward():
-    GPIO.output(left_motor_parameters["in"][0], GPIO.LOW)
-    GPIO.output(left_motor_parameters["in"][1], GPIO.HIGH)
+    left_motor.ChangeDutyCycle(math.fabs(left))
+    right_motor.ChangeDutyCycle(math.fabs(right))
