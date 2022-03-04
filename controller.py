130f609a46d604ad66a339f3ae8a90e837e34b78
Yanni Pang <yanni@bu.edu>
2022-03-03 20:19:43 -0500

    switch to websockets

--- controller.py
+++ controller.py
@@ -1,31 +1,27 @@
-import socket
 import json
-import math
+import asyncio
+import websockets
 import motor
 
-s = socket.socket()
-print("Created Socket")
-port = 12345
-s.bind(('', port))
-print(f"socket binded to {port}")
-s.listen(5)
-print("socket is listening")
 
-
-while True:
-    c, addr = s.accept()
-    print(f"Connected to {addr}")
-    c.send("OK".encode())
-    while (1):
+async def handler(websocket):
+    while True:
         try:
-            result = json.loads(c.recv(1024).decode())
-            print(result)
+            message = await websocket.recv()
+        except websockets.ConnectionClosedOK:
+            break
+        try:
+            result = json.loads(message)
+            print(f"degrees: {result['degrees']}, distance: {result['distance']}")
+            motor.drive(result["degrees"], result["distance"])
         except:
             continue
-        # x = result["distance"]*math.cos(result["degrees"])
-        # y = result["distance"] * math.sin(result["degrees"])
-        # print(f"x: {x}, y:{y}")
-        motor.drive(result["degrees"], result["distance"])
+        # print(message)
 
-    # c.close()
-    # break
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
