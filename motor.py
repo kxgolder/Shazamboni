8d1fbb637ebaadc8d224c386b7d5904ffa2f6408
Yanni Pang <yanni@bu.edu>
2022-03-29 10:13:17 -0400

    added comments

--- motor.py
+++ motor.py
@@ -8,7 +8,6 @@ from settings import *
 
 from time import sleep
 
-
 left_motor_parameters = {
     "in": [L_IN_1, L_IN_2],
     "en": L_EN
@@ -31,15 +30,14 @@ left_motor.start(25)
 right_motor.start(25)
 
 
-
 def drive(degrees, distance):
     right, left = d.joystickToDiff(degrees, distance, MIN_JOYSTICK, MAX_JOYSTICK, MIN_SPEED, MAX_SPEED)
     if right > 0:
-
+        # Drive the motor clockwise
         GPIO.output(right_motor_parameters["in"][0], GPIO.HIGH)
         GPIO.output(right_motor_parameters["in"][1], GPIO.LOW)
     elif right < 0:
-
+        # Drive the motor counter-clockwise
         GPIO.output(right_motor_parameters["in"][0], GPIO.LOW)
         GPIO.output(right_motor_parameters["in"][1], GPIO.HIGH)
     else:
@@ -47,10 +45,11 @@ def drive(degrees, distance):
         GPIO.output(right_motor_parameters["in"], GPIO.LOW)
 
     if left > 0:
-
+        # Drive the motor clockwise
         GPIO.output(left_motor_parameters["in"][0], GPIO.HIGH)
         GPIO.output(left_motor_parameters["in"][1], GPIO.LOW)
     elif left < 0:
+        # Drive the motor counter-clockwise
 
         GPIO.output(left_motor_parameters["in"][0], GPIO.LOW)
         GPIO.output(left_motor_parameters["in"][1], GPIO.HIGH)
@@ -58,5 +57,6 @@ def drive(degrees, distance):
 
         GPIO.output(left_motor_parameters["in"], GPIO.LOW)
 
+    # Set speed of motor
     left_motor.ChangeDutyCycle(math.fabs(left))
     right_motor.ChangeDutyCycle(math.fabs(right))
