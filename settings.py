8d5f7b5726742b244640a24005e657117c6ec5a2
Yanni Pang <yanni@bu.edu>
2022-03-31 10:38:05 -0400

    Document settings

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+"""
+Motor Settings
+"""
+
 # Left Motor Pins
 L_IN_1 = 26
 L_IN_2 = 19
@@ -11,11 +15,21 @@ R_EN = 12
 # PWM Frequency
 PWM_FREQ = 100
 
+"""
+Differential Driving Settings
+"""
 # Joystick Parameters and Speed Definitions
 MAX_JOYSTICK = 1
 MIN_JOYSTICK = -1
 MAX_SPEED = 100
 MIN_SPEED = -100
+
+
+"""
+Ultrasonic Sensor Settings
+"""
+# Distance Threshold (cm)
+US_THRESHOLD = 7
 
 # GPIO Pins for Ultrasonic Sensors
 # Front Ultrasonic Sensor
@@ -23,5 +37,5 @@ F_GPIO_TRIGGER = 18
 F_GPIO_ECHO = 24
 
 # Back Ultrasonic Sensor
-B_GPIO_TRIGGER_2 = 22
-B_GPIO_ECHO_2 = 23
+B_GPIO_TRIGGER = 22
+B_GPIO_ECHO = 23
