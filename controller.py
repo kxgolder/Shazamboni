90e07ff6496372680d9ce09a75f36609e04e4faf
Yanni Pang <yanni@bu.edu>
2022-03-31 10:38:29 -0400

    add code for ultrasonics

--- controller.py
+++ controller.py
@@ -2,12 +2,47 @@ import json
 import asyncio
 import websockets
 import motor
+import ultrasonic_sensors as u
 import RPi.GPIO as GPIO
+from settings import *
+import time
+
+
+def check_distance(trigger, echo):
+    if u.distance(trigger, echo) <= US_THRESHOLD:
+        # Stop vehicle from moving
+        motor.drive(0, 0)
+
+        # if both sensors tripped threshold, vehicle is stuck
+        if u.distance(F_GPIO_TRIGGER, F_GPIO_ECHO) <= US_THRESHOLD and u.distance(B_GPIO_TRIGGER,
+                                                                                  B_GPIO_ECHO) <= US_THRESHOLD:
+            print("Vehicle is stuck. Remove obstacles or adjust position")
+            while True:
+                # if vehicle is adjusted to be ok, return
+                if u.distance(F_GPIO_TRIGGER, F_GPIO_ECHO) >= US_THRESHOLD and u.distance(B_GPIO_TRIGGER,
+                                                                                          B_GPIO_ECHO) >= US_THRESHOLD:
+                    print("Vehicle adjusted.")
+                    return
+        else:
+            # If front ultrasonic is tripped, reverse vehicle
+            if trigger == F_GPIO_TRIGGER:
+                motor.drive(180, 0.5)
+            else:
+                # else read ultrasonic was triggered, advance vehicle
+                motor.drive(0, 0.5)
+
+            while True:
+                # Stop vehicle after threshold is passed
+                if u.distance(trigger, echo) >= US_THRESHOLD:
+                    motor.drive(0, 0)
+                    return
+
 
 async def handler(websocket):
     print(f"Client connected: {websocket.remote_address}")
     await websocket.send("Connected to server.")
     while True:
+
         try:
             message = await websocket.recv()
             print(message)
@@ -17,6 +52,10 @@ async def handler(websocket):
         try:
             result = json.loads(message)
             print(f"degrees: {result['degrees']}, distance: {result['distance']}")
+
+            check_distance(F_GPIO_TRIGGER, F_GPIO_ECHO)
+            check_distance(B_GPIO_TRIGGER, B_GPIO_ECHO)
+
             motor.drive(result["degrees"], result["distance"])
         except:
             continue
@@ -24,11 +63,14 @@ async def handler(websocket):
 
 
 async def main():
-    print("Initializing motor")
+    print("Initializing ultrasonic sensors")
+    u.init()
+    print("Initializing motors")
     motor.init()
     print("Initializing websocket server")
     async with websockets.serve(handler, "", 8001):
         await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     try:
---
+++ ultrasonic_sensors.py
@@ -0,0 +1,52 @@
+# Libraries
+import RPi.GPIO as GPIO
+from settings import *
+import time
+
+
+# set GPIO direction (IN / OUT)
+def init():
+    GPIO.setup(F_GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(F_GPIO_ECHO, GPIO.IN)
+    GPIO.setup(B_GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(B_GPIO_ECHO, GPIO.IN)
+
+
+def distance(trigger, echo):
+    # set Trigger to HIGH
+    GPIO.output(trigger, True)
+    # GPIO.output(GPIO_TRIGGER_2, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(trigger, False)
+    # GPIO.output(GPIO_TRIGGER_2, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # StartTime_2 = time.time()
+    # StopTime_2 = time.time()
+    # save StartTime
+    while GPIO.input(echo) == 0:
+        StartTime = time.time()
+
+    # while GPIO.input(GPIO_ECHO_2) == 0:
+    # StartTime_2 = time.time()
+    # save time of arrival
+    while GPIO.input(echo) == 1:
+        StopTime = time.time()
+
+    # while GPIO.input(GPIO_ECHO_2) == 1:
+    # StopTime_2 = time.time()
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # TimeElapsed2 = StopTime_2 - StartTime_2
+
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+
+    distance = (TimeElapsed * 34300) / 2
+    # distance2 = (TimeElapsed2 * 34300) / 2
+
+    return distance
