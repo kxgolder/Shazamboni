b632b83f3e98453653304eefee6c871fd6c69514
Yanni Pang <yanni@bu.edu>
2022-04-06 15:01:00 -0400

    try new ultrasonic method and multiprocessing

--- controller.py
+++ controller.py
@@ -4,47 +4,49 @@ import websockets
 import motor
 import ultrasonic_sensors as u
 import RPi.GPIO as GPIO
+from multiprocessing import Process, Value
+from gpiozero import DistanceSensor
 from settings import *
 import time
 
-def check_distance(trigger1, trigger2, echo1, echo2, back_dist, front_dist):
-    print("Checking distance")
-    if back_dist <= US_THRESHOLD or front_dist <= US_THRESHOLD:
+f_state = None
+r_state = None
 
-        # Stop vehicle from moving
-        print("Stopping vehicle")
-        motor.drive(0, 0)
 
-        # if both sensors tripped threshold, vehicle is stuck
-        if u.distance(F_GPIO_TRIGGER, F_GPIO_ECHO) <= US_THRESHOLD and u.distance(B_GPIO_TRIGGER,
-                                                                                  B_GPIO_ECHO) <= US_THRESHOLD:
-            print("Vehicle is stuck. Remove obstacles or adjust position")
-            while True:
-                # if vehicle is adjusted to be ok, return
-                if u.distance(F_GPIO_TRIGGER, F_GPIO_ECHO) > US_THRESHOLD and u.distance(B_GPIO_TRIGGER,
-                                                                                         B_GPIO_ECHO) > US_THRESHOLD:
-                    print("Vehicle adjusted.")
-                    return 1
-        else:
-            # If front ultrasonic is tripped, reverse vehicle
-            if front_dist <= US_THRESHOLD:
-                print("Front ultrasonic tripped, reversing vehicle")
-                motor.drive(180, 1)
-            else:
-                # else read ultrasonic was triggered, advance vehicle
-                print("Rear ultrasonic tripped, advancing vehicle")
-                motor.drive(0, 1)
+def front_ultrasonic_detection(a):
+    front_ultrasonic = DistanceSensor(echo=F_GPIO_ECHO, trigger=F_GPIO_TRIGGER)
+    while True:
+        front_ultrasonic.wait_for_in_range()
+        print("Front ultrasonic tripped, reversing vehicle")
+        a.value = "triggered"
+        motor.drive(180, 1)
+        front_ultrasonic.wait_for_out_of_range()
+        print("Out of range")
+        if a.value == "triggered":
+            motor.drive(0, 0)
+        a.value = "clear"
 
-            while True:
-                # Stop vehicle after threshold is passed
-                if u.distance(trigger1, echo1) > US_THRESHOLD + US_ADDITIONAL_DISTANCE and u.distance(trigger2, echo2) > US_THRESHOLD + US_ADDITIONAL_DISTANCE:
-                    motor.drive(0, 0)
-                    return 1
+
+def rear_ultrasonic_detection(a):
+    rear_ultrasonic = DistanceSensor(echo=B_GPIO_ECHO, trigger=B_GPIO_TRIGGER)
+    while True:
+        rear_ultrasonic.wait_for_in_range()
+        print("Rear ultrasonic tripped, advancing vehicle")
+        a.value = "triggered"
+        motor.drive(0, 1)
+        rear_ultrasonic.wait_for_out_of_range()
+        print("Out of range")
+        if a.value == "triggered":
+            motor.drive(0, 0)
+        a.value = "clear"
 
 
 async def handler(websocket):
     print(f"Client connected: {websocket.remote_address}")
     await websocket.send("Connected to server.")
+    global f_state
+    global r_state
+
     while True:
 
         try:
@@ -55,21 +57,13 @@ async def handler(websocket):
             break
         try:
             result = json.loads(message)
-            print(f"degrees: {result['degrees']}, distance: {result['distance']}")
-            # check_distance(F_GPIO_TRIGGER, F_GPIO_ECHO)
-            front_dist = u.distance(F_GPIO_TRIGGER, F_GPIO_ECHO)
-            back_dist = u.distance(B_GPIO_TRIGGER, B_GPIO_ECHO)
-            print(back_dist, front_dist)
-            if back_dist <= US_THRESHOLD or front_dist <= US_THRESHOLD:
-                print("SENDING 1")
-                await websocket.send("1")
-            if not (check_distance(B_GPIO_TRIGGER, F_GPIO_TRIGGER, B_GPIO_ECHO, F_GPIO_ECHO, back_dist, front_dist)):
+
+            if f_state == "clear" and r_state == "clear":
                 motor.drive(result["degrees"], result["distance"])
             else:
-                print("Dropped packet")
-                print("SENDING 0")
-                await websocket.send("0")
-        except:
+                continue
+
+        except json.JSONDecodeError:
             continue
         # print(message)
 
@@ -77,6 +71,15 @@ async def handler(websocket):
 async def main():
     print("Initializing ultrasonic sensors")
     u.init()
+    global f_state
+    global r_state
+    f_state = Value('s', "clear")
+    r_state = Value('s', "clear")
+    p = Process(target=front_ultrasonic_detection, args=f_state)
+    p1 = Process(target=rear_ultrasonic_detection, args=r_state)
+    p.start()
+    p1.start()
+
     print("Initializing motors")
     motor.init()
     print("Initializing websocket server")
@@ -85,6 +88,7 @@ async def main():
 
 
 if __name__ == "__main__":
+
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
